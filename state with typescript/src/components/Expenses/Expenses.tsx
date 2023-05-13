e0f59ac03a5f499fa1f0ac0df2edb1c940c91ed3
starplanter93 <starplanter93@gmail.com>
2023-05-13 23:30:17 +0900

    상태끌어올리기 복습

--- state with typescript/src/components/Expenses/Expenses.tsx
+++ state with typescript/src/components/Expenses/Expenses.tsx
@@ -4,14 +4,19 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import { ExpensesType } from "../../model/expeses";
+import ExpensesFilter from "./ExpensesFilter";
 
 interface OwnProps {
   items: ExpensesType;
 }
 
 function Expenses({ items }: OwnProps) {
+  function selectYearHandler(selectedYear: string) {
+    console.log(selectedYear);
+  }
   return (
     <Card className="expenses">
+      <ExpensesFilter onSelectYear={selectYearHandler} />
       {items.map((item) => (
         <ExpenseItem
           key={item.id}
