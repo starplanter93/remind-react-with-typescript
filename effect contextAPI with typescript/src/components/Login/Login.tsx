d67ee6ea48d1b6acf1b72b82b65df092b8c8898a
starplanter93 <starplanter93@gmail.com>
2023-05-15 20:34:59 +0900

    cleanup함수

--- effect contextAPI with typescript/src/components/Login/Login.tsx
+++ effect contextAPI with typescript/src/components/Login/Login.tsx
@@ -16,9 +16,15 @@ function Login({ onLogin }: OwnProps) {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
